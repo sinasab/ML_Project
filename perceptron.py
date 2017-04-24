3a06a4b9397b834b926a4e17db1956e72cb8eb48
Sina Sabet <sinasabet81@gmail.com>
2017-04-23 23:12:21 -0400

    Added simple perceptron implementation

---
+++ perceptron.py
@@ -0,0 +1,25 @@
+from sklearn.linear_model import Perceptron
+import numpy as np
+
+def perceptron(features, labels):
+    p = Perceptron(
+        n_iter=200,
+        # verbose=True,
+        # n_jobs=-1, # parallelize
+        shuffle=True,
+        random_state=0
+    )
+    p.fit(features["train"], labels["train"])
+    print p.score(features["test"], labels["test"])
+    return p
+
+if __name__ == "__main__":
+    from getdata import getSplitData
+    from getlabels import instrumentsToLabels
+    from featurize import mfcc_averages
+
+    splitData = getSplitData()
+
+    labels = instrumentsToLabels(splitData)
+    features = mfcc_averages(splitData)
+    p = perceptron(features, labels)
