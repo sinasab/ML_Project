12e8071e45fc2359766a18d8364f3d3662e561ec
Cameron Long <long.1314@osu.edu>
2017-04-26 10:46:44 -0400

    added oboe and Bb clarinet data, and added 2 classifiers

--- perceptron.py
+++ perceptron.py
@@ -1,14 +1,30 @@
-from sklearn.linear_model import Perceptron
+from sklearn.multiclass import OneVsOneClassifier
 import numpy as np
+import sys
 
 def perceptron(features, labels):
-    p = Perceptron(
+    from sklearn.linear_model import Perceptron
+    p = OneVsOneClassifier(Perceptron(
         n_iter=200,
         # verbose=True,
         # n_jobs=-1, # parallelize
         shuffle=True,
         random_state=0
-    )
+    ))
+    p.fit(features["train"], labels["train"])
+    print p.score(features["test"], labels["test"])
+    return p
+
+def svm(features, labels):
+    from sklearn.svm import SVC
+    p = OneVsOneClassifier(SVC(kernel='poly'))
+    p.fit(features["train"], labels["train"])
+    print p.score(features["test"], labels["test"])
+    return p
+
+def randomforest(features, labels):
+    from sklearn.ensemble import RandomForestClassifier
+    p = RandomForestClassifier(n_estimators=20)
     p.fit(features["train"], labels["train"])
     print p.score(features["test"], labels["test"])
     return p
@@ -17,9 +33,16 @@ if __name__ == "__main__":
     from getdata import getSplitData
     from getlabels import instrumentsToLabels
     from featurize import mfcc_averages
-
     splitData = getSplitData()
-
     labels = instrumentsToLabels(splitData)
     features = mfcc_averages(splitData)
-    p = perceptron(features, labels)
+    if len(sys.argv) == 2:
+        classifier = int(sys.argv[1])
+        if classifier == 1:
+            p = svm(features, labels)
+        elif classifier == 2:
+            p = randomforest(features, labels)          
+        else:
+            p = perceptron(features, labels)
+    else:
+        p = perceptron(features, labels)
